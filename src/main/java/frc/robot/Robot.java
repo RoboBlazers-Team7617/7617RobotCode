662897cb27a46fba99c05fac8fff408ec7954955
Caine Ardayfio <caineardayfio@2021-Caine-Ardayfio.local>
2019-05-07 12:32:22 -0400

    Initial Code

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,56 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.ExampleSubsystem;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PWMVictorSPX;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+public class Robot extends TimedRobot {
+  PWMVictorSPX m_frontLeft = new PWMVictorSPX(1);
+  PWMVictorSPX m_rearLeft = new PWMVictorSPX(2);
+  SpeedControllerGroup m_left = new SpeedControllerGroup(m_frontLeft, m_rearLeft);
+  PWMVictorSPX m_frontRight = new PWMVictorSPX(3);
+  PWMVictorSPX m_rearRight = new PWMVictorSPX(4);
+  SpeedControllerGroup m_Right = new SpeedControllerGroup(m_frontRight, m_rearRight);
+  DifferentialDrive m_drive = new DifferentialDrive(m_left, m_right);
+  
+  private final Joystick m_stick = new Joystick(0);
+  private final Timer m_timer = new Timer();
+
+
+  @Override
+  public void autonomousInit() {
+    m_timer.reset();
+    m_timer.start();
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+    // Drive for 2 seconds
+    if (m_timer.get() < 2.0) {
+      m_robotDrive.arcadeDrive(0.5, 0.0); // drive forwards half speed
+    } else {
+      m_robotDrive.stopMotor(); // stop robot
+    }
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    m_robotDrive.arcadeDrive(m_stick.getY(), m_stick.getX());
+  }
+}
